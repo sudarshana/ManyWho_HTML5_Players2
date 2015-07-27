aca4bd405a96a4559788c25b03ad16380bf76b34
Joao Moreira <joao.moreira@manywho.com>
2015-07-27 16:29:51 +0100

    CORE-817 - Added toggling to the image insertion component

--- runtime/js/components/content.js
+++ runtime/js/components/content.js
@@ -39,7 +39,7 @@ permissions and limitations under the License.
                         icon: 'image',
                         onclick: function () {
 
-                            self.setState({ isImageUploadOpen: true });
+                            self.setState({ isImageUploadOpen: !self.state.isImageUploadOpen });
                             self.render();
 
                         }
